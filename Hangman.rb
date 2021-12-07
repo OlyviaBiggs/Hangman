5d43452aefa4ce8097cf98620c5f3996f96b0f9a
OlyviaBiggs <olyvia@biggs.email>
2021-12-07 10:14:37 -0500

    ITS WORKING!!! YASSSSSSS

--- Hangman.rb
+++ Hangman.rb
@@ -1,5 +1,5 @@
 class Person 
-    def put_head
+    def head
         putc(" ")
         putc(" ")
         putc("_")
@@ -24,7 +24,7 @@ class Person
         putc("|")
     end
 
-    def put_body
+    def body
         putc("           ")
         putc("      ")
         putc("      ")
@@ -49,7 +49,7 @@ class Person
         putc(?\n)
     end
 
-    def put_legs
+    def legs
         putc(" ")
         putc(" ")
         putc("/")
@@ -74,13 +74,100 @@ end
 class User
 end
 
+#mocha = Words.new.make_a_rat
+
 print 'Do ya wana use the Hangman thing?   Y or N'
 userAgree = gets.chomp
 if userAgree == 'Y' or userAgree == 'y'
   userAgree = true
+    puts 'Then we are gonna use the Hangman thing.'
+    puts 'The word is three letters, none of the letters repeat. Q or W or E or R or T or Y or U or I or O or P or A or S or D or F or G or H or J or K or 
+  L or Z or X or C or V or B or N or M'
+  userAgree = gets.chomp
+  if userAgree == 'R' or userAgree == 'r'
+    userAgree == true
+        puts 'Good job! You got a letter in there. woo-hoo.'
+    else 
+        userAgree == false
+            puts 'Nope! Not even close! Now put a head...'
+            putc(" ")
+            putc(" ")
+            putc("_")
+            putc("_")
+            putc("_")
+            putc(?\n)
+            putc(" ")
+            putc("|")
+            putc(" ")
+            putc(" ")
+            putc(" ")
+            putc("|")
+            putc(" ")
+            putc(" ")
+            putc(" ")
+            putc(?\n)
+            putc(" ")
+            putc("|")
+            putc("_")
+            putc("_")
+            putc("_")
+            putc("|")
+      end
+        puts 'Now the next one... Q or W or E or T or Y or U or I or O or P or A or S or D or F or G or H or J or K or 
+        L or Z or X or C or V or B or N or M'
+        userAgree = gets.chomp
+        if userAgree == 'A' or userAgree == 'a'
+            puts "yippee. you did it."
+            puts "ra"
+  else 
+    userAgree == false
+        puts 'NOPE NOPE AND NOPE! Time for the head... or body.. or arms.. or WHO CARES? I HATE THIS GAME SO IT WILL GO QUICK.'
+        putc(" ")
+        putc(" ")
+        putc("_")
+        putc("_")
+        putc("_")
+        putc(?\n)
+        putc(" ")
+        putc("|")
+        putc(" ")
+        putc(" ")
+        putc(" ")
+        putc("|")
+        putc(" ")
+        putc(" ")
+        putc(" ")
+        putc(?\n)
+        putc(" ")
+        putc("|")
+        putc("_")
+        putc("_")
+        putc("_")
+        putc("|")
+        putc("           ")
+        putc("      ")
+        putc("      ")
+        putc(?\n)
+        putc(" ")
+        putc(" ") 
+        putc("/")
+        putc("|")
+        putc("\\")
+        putc(?\n)
+        putc(" ")
+        putc("/")
+        putc(" ")
+        putc("|")
+        putc(" ")
+        putc("\\")
+        putc(?\n)
+        putc("           ")
+        putc("      ")
+        putc("      ")
+        putc("|")
+        putc(?\n)
+  end
 else
   userAgree = false
+  puts 'Then ya should get out.'
 end
-
-mocha = Words.new.make_a_rat
-
