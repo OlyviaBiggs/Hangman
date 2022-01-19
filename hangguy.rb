689d1a06326b4adc5e1fd12ee0dc50c48a8a2b46
OlyviaBiggs <olyvia@biggs.email>
2022-01-19 12:01:39 -0500

    Fixed input, output, exiting, and other useful things I of course forgot... -fReAk

--- hangguy.rb
+++ hangguy.rb
@@ -1,4 +1,4 @@
-print 'Do you want to use the Hangman thing?   Y or N
+print 'Do you want to play Hangman?   Y or N
 '
 userAgree = gets.chomp
 if userAgree == 'Y' or userAgree == 'y'
@@ -11,17 +11,28 @@ end
 
 puts 'The word is three letters, none of the letters repeat.'
 userAgree = gets.chomp
-if userAgree == 'R' or userAgree == 'r'
-userAgree == true
-    puts 'Good job! You got a letter in there. woo-hoo.'
-end
-
-if userAgree == 'A' or userAgree == 'a'
-    userAgree == true
-    puts 'Congrats! You got ONE letter in! Kinda good!'
-end
-
-if userAgree == 'T' or userAgree == 't'
-    userAgree == true
-    puts 'Yippee-doo-dah-dai! You got a letter, now pick another!'
-end
+#if userAgree == 'R' or userAgree == 'r' or userAgree == 'A' or userAgree == 'a' or userAgree == 'T' or userAgree == 't'
+#userAgree == true
+#puts 'Good job! You selected the right letter.'
+ #   puts '"r__" Please select another letter.'
+    userAgree = gets.chomp 
+    if userAgree == 'R' or userAgree == 'r'
+        puts 'Good job you guessed the letter "r."  you have "r__" found.'
+    else
+        gets.chomp
+        if userAgree == 'A' or userAgree == 'a' 
+            userAgree == true
+            puts 'Good job! You selected the right letter.'
+            puts '"_a_"Select another letter now.'
+            userAgree = gets.chomp
+           # if userAgree == 'T' or userAgree == 't'
+            #    userAgree == true
+             #   puts 'Congratulations! You spelled the word "rat"'
+              #  exit
+        else
+            gets.chomp
+            if userAgree == 'T' or userAgree == 't'
+                puts 'Good. You have "__t"'
+            end
+        end
+    end
