b40b3b7b26767e6428a28213de3442c4d4857f9d
OlyviaBiggs <olyvia@biggs.email>
2022-01-20 11:59:27 -0500

    I hath addeth two new extensions! Noweth, I hath puteth two new thing-a-mah-jigs. HATH YE GOOD EVENING! ~FrEaK

--- Hangman.rb
+++ Hangman.rb
@@ -11,7 +11,6 @@ end
 
 
 
-
 puts 'The word is three letters, none of the letters repeat.'
 userAgree = gets.chomp
 if userAgree == 'R' or userAgree == 'r' 
@@ -44,4 +43,43 @@ if userAgree == 'R' or userAgree == 'r'
             exit
         end
     end
+    userAgree = gets.chomp
+elsif 
+    if userAgree == 'A' or userAgree == 'a'
+        puts 'Great job! You selected the right letter!'
+        puts '_a_'
+        puts 'Now pick another!'
+        userAgree = gets.chomp
+        if userAgree == 'T' or userAgree == 't'
+            puts 'Fantastic!'
+            puts '_at'
+            puts 'Now another!'
+            userAgree = gets.chomp
+            if userAgree == 'R' or userAgree == 'r'
+                puts 'Wonderful! You won!'
+                puts 'rat'
+                puts 'Goodbye now.'
+                exit
+            end
+        end
+    end
+    userAgree = gets.chomp
+elsif  
+    if userAgree == 'T' or userAgree == 't'
+        puts 'Terrific! You have achieved-eth to calculate-eth the correct digit-eth!'
+        puts '__t'
+        puts 'Now calculate-eth what the next digit-eth be-eth!'
+        userAgree = gets.chomp
+        if userAgree == 'A' or userAgree == 'a'
+            puts 'Superb! You yet agāin achieved-eth the digit-eth!'
+            puts '_at'
+            userAgree = gets.chomp
+            if userAgree == 'R' or userAgree == 'r'
+                puts 'Ye hath completed-eth the word-eth! Claim-eth yer reward, Knight-eth!'
+                puts 'rat .... e t h'
+                exit
+            end
+        end
+    end
 end
+    
